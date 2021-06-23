5f1d6ad4ee264f9d6164a4709501ff6738a18e03
Delacyr Ferreira <delacyr@gmail.com>
2021-06-23 01:36:15 -0400

    Auth with firebase, routes, navigation, contexts and hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import { Button } from "./components/Button";
+import { useEffect } from 'react';
+import { createContext, useState } from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
+import { AuthContextProvider } from './contexts/AuthContext';
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { firebase, auth } from './services/firebase';
 
 function App() {
   return (
-    <div>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-    </div>
-
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
-
 export default App;
---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,41 @@
+import { Button } from '../components/Button';
+import { Link } from 'react-router-dom';
+
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+
+import '../styles/auth.scss';
+// import { useContext } from 'react';
+// import { AuthContext } from '../contexts/AuthContext';
+
+export function NewRoom() {
+    //const { user } = useContext(AuthContext);
+
+    return (
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="" />
+                <strong>Crie salas de Q&amp;A ao vivo</strong>
+                <p>Tire as dúvidas da sua audiência em tempo real</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="" />
+                    <h2>Criar uma nova sala</h2>
+                    <form>
+                        <input
+                            type="text"
+                            placeholder="Nome da sala"
+                        />
+                        <Button type="submit">
+                            Criar sala
+                        </Button>
+                    </form>
+                    <p>
+                        Quer entrar em uma sala existe? <Link to="/">clicke aqui</Link>
+                    </p>
+                </div>
+            </main>
+        </div>
+    )
+}
